5767ea6bc9dff07dd8420bae700f97303ebf4f1f
ivy <px2553@gmail.com>
2020-05-30 16:53:16 +0800

    fff

--- FitMate/src/FirstTest.java
+++ FitMate/src/FirstTest.java
@@ -4,6 +4,6 @@ public static void main(String[] args) {
 	System.out.println("......是在哈囉!");
 	System.out.println("......是在哈囉!");
 	System.out.println("......是在哈囉!");
-	System.out.println("......是在哈囉!");
+
 }
 }
