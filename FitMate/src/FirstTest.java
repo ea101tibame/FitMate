43aa308cac5c48be8cc42b684b935862c0a50a4d
ivy <px2553@gmail.com>
2020-05-30 16:51:53 +0800

    hiii

--- FitMate/src/FirstTest.java
+++ FitMate/src/FirstTest.java
@@ -2,5 +2,8 @@
 public class FirstTest {
 public static void main(String[] args) {
 	System.out.println("......是在哈囉!");
+	System.out.println("......是在哈囉!");
+	System.out.println("......是在哈囉!");
+	System.out.println("......是在哈囉!");
 }
 }
