fb6db76bb202d7e6dd5338b541cd143f2102c32d
Tayli <ea101.tibame@gmail.com>
2020-06-09 21:47:18 +0800

    LESSON DAO INSERT 一次新增三個表單

--- src/com.lesson.model/LessonDAO_interface.java
+++ src/com.lesson.model/LessonDAO_interface.java
@@ -7,14 +7,22 @@ import org.json.JSONArray;
 import test.expertise.model.ExpertiseVO;
 
 public interface LessonDAO_interface {
+	
 	public void insert(LessonVO lessonVO);
+	
 	public void update(LessonVO lessonVO);//備註:沒有刪除 只變更狀態為下架
+	
+	public LessonVO getALessonByLessno(String lessno);
+	
 	/*此運動種類有多少課程 用類型查*/
 	public List<LessonVO> findLessonByLessonType(String lesstype);
+	
 	/*查此運動類型的詳細描述 要用到EXPERTISE的VO*/
 	public ExpertiseVO getExpByExpno(String expno);
+	
 	/*查詢全部課程 課程總覽*/
 	public List<LessonVO> getAll();
+	
 	/*查詢某教練的課程資訊(一對多)(回傳一串 JSONArray 放入課表用)*/
 	public JSONArray getCoachAllLesson(String coano);
 }
